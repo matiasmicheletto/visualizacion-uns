a4a865a9e13115893acebe4d219de79f0c051dac
Matias <matiasmicheletto@gmail.com>
2021-01-05 12:46:12 -0300

    Mejoras scrapper

--- src/data/scrapper.py
+++ src/data/scrapper.py
@@ -2,7 +2,7 @@ from urllib.request import Request, urlopen
 import bs4
 import os
 import csv
-from random import uniform
+from random import uniform, sample
 from time import sleep
 
 
@@ -10,25 +10,35 @@ from time import sleep
 url = 'https://www.brewersfriend.com/homebrew-recipes/page/'
 row_len = 10 # Number of columns that each row must have to match the desired data
 start_page = 1 # Scraping range from
-end_page = 10 # Scraping range to (add one)
+end_page = 200 # Scraping range to (add one)
 output_file_name = 'scraped_data.csv'
 del_if_exists = True # Delete the log file before start if already exist
+request_timeout = 10 # If the request takes too much time, stop scrapper
 
-# Number of columns of interest
+# Indexes of columns of interest (may change on website version)
 col_numbers = {
-    'name': 0,
-    'style': 1,
+    'name': 0, # Recipe name (may contain commas)
+    'style': 1, # Recipe style (may contain commas)
     'abv': 5,
     'ibu': 6,
     'color': 7
 }
 
 
-def request_page(url, page_num): # Request page and return data table as array
+def load_user_agents(): # Load a list of user agents from txt file
+    ua = []
+    with open('user-agents.txt', 'r') as fd:
+        reader = csv.reader(fd)
+        for row in reader:
+            ua.append(row[0])
+    return ua 
+
+
+def request_page(url, page_num, user_agent): # Request page and return data table as array
     pn = str(page_num)
-    print('Requesting page ' + pn + '...')
-    req = Request(url + pn, headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'})
-    content = urlopen(req, timeout = 10).read()
+    print('Requesting page ' + pn + ' -- (UA = ' + user_agent + ')...', end = '')
+    req = Request(url + pn, headers = {'User-Agent': user_agent, 'Referer': 'https://www.google.com'})
+    content = urlopen(req, timeout = request_timeout).read()
 
     data = []
 
@@ -42,26 +52,35 @@ def request_page(url, page_num): # Request page and return data table as array
             col_numbers = row.find_all('td')
             col_numbers = [ele.text.strip() for ele in col_numbers]
             data.append([ele for ele in col_numbers if ele])
+        
+        print(' Done.')
     else:
-        print('Empty page or request timed out')
+        print('\nEmpty page or request timed out')
 
     return data
 
 
 def extract_columns(row, col_numbers): # Extract the columns of interest
-    return [ row[col_numbers['name']], row[col_numbers['style']], row[col_numbers['abv']], row[col_numbers['ibu']], row[col_numbers['color']] ]
+    r = [ 
+        row[col_numbers['name']], 
+        row[col_numbers['style']], 
+        row[col_numbers['abv']], 
+        row[col_numbers['ibu']], 
+        row[col_numbers['color']] 
+    ]
 
+    for k,el in enumerate(r): # Remove commas
+        r[k] = r[k].replace(',','')
 
-def random_pause(): # Random pause to simulate human user
-    sleep(uniform(1,10))
+    return r
 
 
 def write_to_file(filename, data): # Write table to csv file
-    print('Writing ' + str(len(data)) + ' lines to file ' + filename + '...')
+    print('Writing ' + str(len(data)) + ' lines to file ' + filename + '...', end = '')
 
     with open(filename, 'a') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames = ['name', 'style', 'abv', 'ibu', 'color'])
-        writer.writeheader()
+        #writer.writeheader()
         for r in data:
             writer.writerow({
                 'name': r[0],
@@ -71,21 +90,27 @@ def write_to_file(filename, data): # Write table to csv file
                 'color': r[4]
             })
         
-    print('Data written.')
+    print(' Done.\n')
 
 
+def random_pause(): # Random pause to simulate human user
+    sleep(uniform(1, 5))
+
+
+# Obtain the user agent list
+ua = load_user_agents()
 
 # Check if file exists before begin
-if os.path.exists(filename) and del_if_exists:
-        print('File already exist, deleting...')
-        os.remove(filename)
+if os.path.exists(output_file_name) and del_if_exists:
+        print('File already exist, deleting...', end = '')
+        os.remove(output_file_name)
+        print(' Done.')
 
 # Start scrapping
 for n in range(start_page, end_page):
-    data = request_page(url, n)
+    data = request_page(url, n, sample(ua,1)[0])
     output = [ extract_columns(d, col_numbers) for d in data if len(d) == row_len ]
     write_to_file(output_file_name, output)
     if n < end_page - start_page - 1:
         random_pause()
 
-        
